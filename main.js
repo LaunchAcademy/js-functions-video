30f5f5007720fcf19db496c08ff3214269beb3ba
Dan Pickett <dan.pickett@launchacademy.com>
2017-04-22 15:36:17 -0400

    starting place for functions video

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+# Learning Goals
+- Declare a function as a means to reduce duplication
+- Declare a function that requires arguments
+- Implement a function that has a return value
+- Demonstrate how functions serve as a “black box” in the context of larger programs
+*/
+
+let nameA = 'Susan'
+console.log(`Hi ${nameA}`)
+
+let nameB = 'Jason'
+console.log(`Hi ${nameB}`)
